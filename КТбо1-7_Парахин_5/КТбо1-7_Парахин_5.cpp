a79e73bca7e9c9f4e84c3914bada01a979abd472
Arseniy Parakhin <senya.paraxin.04@mail.ru>
2023-05-03 17:35:59 +0300

    Check_Input + Choice

--- КТбо1-7_Парахин_5/КТбо1-7_Парахин_5.cpp
+++ КТбо1-7_Парахин_5/КТбо1-7_Парахин_5.cpp
@@ -10,17 +10,48 @@ XX.05.2023
 #include <fstream>
 #include <vector>
 #include <map>
+#include <set>
 using namespace std;
+
+typedef struct Cell {
+    char content = 'Х';
+    bool knob = false;
+} Cell;
+
+bool Check_Input(string Input);
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int flag = 1;
+    
+    string Input_string;
+    vector <Cell> Tape;
 
+    bool flag = 1;
     do {
-       
+        cout << "Введите входные данные: ";
+        cin >> Input_string;
 
+        if (Check_Input(Input_string)) {
+
+        }
+        else {
+            cout << "Неправильная строка" << endl;
+        }
+
+        cout << "Хотите продолжить? ";
+        cin >> flag;
     } while (flag == 1);
 
     return 0;
+}
+
+bool Check_Input(string Input) {
+    bool answer = true;
+    for (auto it = Input.begin(); it != Input.end(); it++) {
+        if (*it != '0' && *it != '1') {
+            answer = false;
+        }
+    }
+    return answer;
 }
